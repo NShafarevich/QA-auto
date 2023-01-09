5d1451bd030081fa07e343ab11abc31a1c4547cb
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-09 14:44:25 +0300

    Delete for3.java

--- for3.java
+++
@@ -1,8 +0,0 @@
-public class for3 {
-    public static void main(String[] args) {
-        for (int r=1; r<= 10; r++) {
-            System.out.println ("3*" + r + "=" + 3*r);
-        }
-    }
-}
-
