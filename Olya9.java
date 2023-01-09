dff672400cd9642a92e98aaa6fdc7a0cd154a93b
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-09 14:27:59 +0300

    Add files via upload

---
+++ Olya9.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Olya9 {
+    public static void main(String[] args) {
+        for (int b=1; b<=100; b++) {
+        if (b%5==0) {
+            System.out.print(b + " ");
+}
+}
+    }
+}
