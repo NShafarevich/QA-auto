7b304f98d087812c4db6296f0c254985689e7f1d
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-09 14:27:29 +0300

    Add files via upload

---
+++ Olya8.java
@@ -0,0 +1,10 @@
+public class Olya8 {
+    public static void main(String[] args) {
+        int r = 1;
+        while (r <= 10) {
+            System.out.println("Task " +r);
+            r = r+1;
+        }
+    }
+}
+
