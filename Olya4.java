381b407e1c9b2f84715382342d8529c4f0a77cb0
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-09 14:25:59 +0300

    Add files via upload

---
+++ Olya4.java
@@ -0,0 +1,10 @@
+public class Olya4 {
+    public static void main(String[] args) {
+        int a = 200;
+        int b = 30;
+        int c = 200*15/100;
+        int sum = 10*c + 2*b;
+        System.out.println("Итоговая цена " + sum);
+    }
+}
+
